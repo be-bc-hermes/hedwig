d271a311f6dc806fa885f9fb7e68f7195cc170ed
meverg <eminvergili96@gmail.com>
2021-07-21 20:30:00 +0300

    Listen price change queue to create notifications

---
+++ src/main/java/com/hermes/notificationservice/messaging/ProductPriceChangeListener.java
@@ -0,0 +1,24 @@
+package com.hermes.notificationservice.messaging;
+
+import com.hermes.notificationservice.models.ProductPriceChange;
+import com.hermes.notificationservice.services.NotificationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author meverg
+ */
+@Component
+public class ProductPriceChangeListener {
+
+  private final NotificationService notificationService;
+
+  public ProductPriceChangeListener(NotificationService notificationService) {
+    this.notificationService = notificationService;
+  }
+
+  @RabbitListener(queues = "${ns.rabbit.queues.product-price-change.name}")
+  public void productPriceChangeListener(ProductPriceChange message) {
+    notificationService.createProductPriceChangeNotification(message);
+  }
+}
