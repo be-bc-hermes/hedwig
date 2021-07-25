77dfcc04ebd265aab2f602f39ef6c0d6b18f3ed4
meverg <eminvergili96@gmail.com>
2021-07-25 22:13:01 +0300

    Add email notification sender

--- src/main/java/com/hermes/notificationservice/services/EmailNotifierService.java
+++ src/main/java/com/hermes/notificationservice/services/EmailNotifierService.java
@@ -1,6 +1,8 @@
 package com.hermes.notificationservice.services;
 
 import com.hermes.notificationservice.entities.Notification;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,8 +11,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailNotifierService {
 
+  private static final String NOREPLY_ADDRESS = "noreply@hedwig.com";
+
+  private final JavaMailSender mailSender;
+
+  public EmailNotifierService(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
+
   public void sendEmailNotification(Notification notification, String emailAddress) {
-    //TODO
-    System.out.printf("Will send email to %s , about %s%n", emailAddress, notification.toString());
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(NOREPLY_ADDRESS);
+    message.setTo(emailAddress);
+    message.setSubject("Product Price Change");
+    message.setText(
+        String.format("Price of the product %d you follow has changed from %.2f to %.2f", notification.getProductId(),
+                      notification.getOldPriceInCents().floatValue() / 100,
+                      notification.getNewPriceInCents().floatValue() / 100));
+    mailSender.send(message);
   }
 }
